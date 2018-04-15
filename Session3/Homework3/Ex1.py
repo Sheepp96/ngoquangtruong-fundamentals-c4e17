7a3d652548d2a0b5a2d8d2695f92fbcf7d198795
Sheepp96 <quangtruong23121996@gmail.com>
2018-04-16 03:53:55 +0700

    initial commit

--- Session3/Homework3/Ex1.py
+++ Session3/Homework3/Ex1.py
@@ -1,21 +1,23 @@
 items = ['T-Shirt', 'Sweater']
 
-print("Welcome to our shop, what do u want?")
-print("Our items: ", end = "")
-print(*items, sep=", ")
+while True:
 
-a = str(input("Enter new iteam: "))
-items.append(a)
-print("Our items: ", end = "")
-print(*items, sep=", ")
+    print("Welcome to our shop, what do u want?")
+    print("Our items: ", end = "")
+    print(*items, sep=", ")
 
-b = int(input("Update position: "))
-c = str(input("Input new item: "))
-items[b] = c
-print("Our items: ", end = "")
-print(*items, sep=", ")
+    a = str(input("Enter new iteam: "))
+    items.append(a)
+    print("Our items: ", end = "")
+    print(*items, sep=", ")
 
-d = int(input("Delete position: "))
-del items[d]
-print("Our items: ", end ="")
-print(*items, sep=", ")
+    b = int(input("Update position: "))
+    c = str(input("Input new item: "))
+    items[b] = c
+    print("Our items: ", end = "")
+    print(*items, sep=", ")
+
+    d = int(input("Delete position: "))
+    del items[d]
+    print("Our items: ", end ="")
+    print(*items, sep=", ")
