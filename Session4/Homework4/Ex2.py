54286274f3e364daf26cd4a00bd7f08843ff840a
Sheepp96 <quangtruong23121996@gmail.com>
2018-04-19 06:09:33 +0700

    initial commit

--- Session4/Homework4/Ex2.py
+++ Session4/Homework4/Ex2.py
@@ -10,14 +10,22 @@ print("'l' if my guess if 'L'arger than your number")
 
 loop = True
 
+n = randint(0,100)
+print("Is ", n, "your number?", end =" ")
+
 while loop:
 
-    n = randint(0,100)
-    print("Is ", n, "your number?", end =" ")
-    input(str(a))
+    c = randint(0,n)
+    d = randint(n,100)
+
+    a = input()
 
     if a == 'c':
         print("Hura - I did it")
+        loop = False
+
     elif a == 's':
-        
+        print("Is ", c, "your number?", end =" ")
+
     elif a == 'l':
+        print("Is ", d, "your number?", end =" ")
